f84f780ceb00aba5e9d4508de2366852aaf2b1ea
Bruno Carter <112636008+brunodev12@users.noreply.github.com>
2023-05-09 15:15:55 -0300

    Update tron_to_eth.py

--- tron_to_eth.py
+++ tron_to_eth.py
@@ -1,8 +1,11 @@
 import base58
 
 def tronToEth(address):
-    if address is not None:
-        if not address.startswith('0x') and not address.startswith('0X'):
-            asc_string = base58.b58decode_check(address)
-            return asc_string.hex().upper().replace("41","0X")
-    return address
+    try:
+        if address is not None:
+            if not address.startswith('0x') and not address.startswith('0X'):
+                asc_string = base58.b58decode_check(address)
+                return asc_string.hex().upper().replace("41","0X")
+        return address
+    except:
+        return None
